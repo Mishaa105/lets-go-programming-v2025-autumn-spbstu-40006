a5d3126dcdfc52efc0116ef421cb66f172796869
Mishaa105 <truhanov2006@mail.ru>
2025-10-04 02:09:33 +0300

    [TASK-2-1] Format fix

--- mikhail.trukhanov/task-2-1/cmd/service/main.go
+++ mikhail.trukhanov/task-2-1/cmd/service/main.go
@@ -3,35 +3,34 @@ package main
 import "fmt"
 
 func main() {
-
 	var (
-		num_of_departments, num_of_workers, temp int
-		sign                                     string
+		numOfDepartments, numOfWorkers, temp int
+		sign                                 string
 	)
-	if _, err := fmt.Scan(&num_of_departments); err != nil {
+	if _, err := fmt.Scan(&numOfDepartments); err != nil {
 		fmt.Println("Invalid input")
 		return
 	}
 
-	for i := 0; i != num_of_departments; i++ {
-		min_temp, max_temp := 15, 30
-		if _, err := fmt.Scan(&num_of_workers); err != nil {
+	for i := 0; i != numOfDepartments; i++ {
+		minTemp, maxTemp := 15, 30
+		if _, err := fmt.Scan(&numOfWorkers); err != nil {
 			fmt.Println("Invalid input")
 			return
 		}
-		for j := 0; j != num_of_workers; j++ {
+		for j := 0; j != numOfWorkers; j++ {
 			if _, err := fmt.Scan(&sign, &temp); err != nil {
 				fmt.Println("Invalid input")
 				return
 			}
-			if sign == ">=" && temp > min_temp {
-				min_temp = temp
+			if sign == ">=" && temp > minTemp {
+				minTemp = temp
 			}
-			if sign == "<=" && temp < max_temp {
-				max_temp = temp
+			if sign == "<=" && temp < maxTemp {
+				maxTemp = temp
 			}
-			if max_temp >= min_temp {
-				fmt.Println(min_temp)
+			if maxTemp >= minTemp {
+				fmt.Println(minTemp)
 			} else {
 				fmt.Println(-1)
 			}
