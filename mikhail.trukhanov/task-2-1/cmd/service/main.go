72333de78b2d7f0af56478e8bace7bea2f987d4c
Mishaa105 <truhanov2006@mail.ru>
2025-10-04 02:33:23 +0300

    [TASK-2-1] Wsl fix

--- mikhail.trukhanov/task-2-1/cmd/service/main.go
+++ mikhail.trukhanov/task-2-1/cmd/service/main.go
@@ -7,28 +7,37 @@ func main() {
 		numOfDepartments, numOfWorkers, temp int
 		sign                                 string
 	)
-	if _, err := fmt.Scan(&numOfDepartments); err != nil {
+
+	_, err := fmt.Scan(&numOfDepartments)
+	if err != nil {
 		fmt.Println("Invalid input")
 		return
 	}
 
 	for i := 0; i != numOfDepartments; i++ {
 		minTemp, maxTemp := 15, 30
-		if _, err := fmt.Scan(&numOfWorkers); err != nil {
+
+		_, err := fmt.Scan(&numOfWorkers)
+		if err != nil {
 			fmt.Println("Invalid input")
 			return
 		}
+
 		for j := 0; j != numOfWorkers; j++ {
-			if _, err := fmt.Scan(&sign, &temp); err != nil {
+			_, err := fmt.Scan(&sign, &temp)
+			if err != nil {
 				fmt.Println("Invalid input")
 				return
 			}
+
 			if sign == ">=" && temp > minTemp {
 				minTemp = temp
 			}
+
 			if sign == "<=" && temp < maxTemp {
 				maxTemp = temp
 			}
+
 			if maxTemp >= minTemp {
 				fmt.Println(minTemp)
 			} else {
